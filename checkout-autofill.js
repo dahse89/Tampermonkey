9f391d1ef062df3b5b317028198ff99e35ce57b0
dahse89 <ph.dahse@gmail.com>
2017-09-13 15:16:59 +0200

    change number

--- checkout-autofill.js
+++ checkout-autofill.js
@@ -19,7 +19,7 @@
         $('#form_firstName').val(hash());
         $('#form_lastName').val(hash());
         $('#form_email').val('philipp.dahse@beauty-spotter.de');
-        $('#form_phone').val('123');
+        $('#form_phone').val('321');
         $('#form_confirm').prop('checked', true);
         $('[type=submit]').focus();
     });
